4dcfeb703e2032cfe1868d668f041b952286fb02
manperez1986-ux <manperez1986@gmail.com>
2026-03-01 15:56:00 -0400

    sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,4 @@
-const CACHE_NAME = 'manu-barber-v70';
-const ASSETS = [
-  'index.html',
-  'manifest.json',
-  'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js',
-  'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js',
-  'https://unpkg.com/lucide@latest'
-];
-
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
-});
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
-});
+const CACHE_NAME = 'manu-barber-v74';
+const ASSETS = ['index.html', 'manifest.json', 'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js', 'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js', 'https://unpkg.com/lucide@latest'];
+self.addEventListener('install', (e) => { e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))); });
+self.addEventListener('fetch', (e) => { e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request))); });
